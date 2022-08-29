4294c8f82f42d5acb8d5d509058f02f292bd6544
Nick Anthony <nickanthony45@gmail.com>
2022-08-29 14:50:37 -0400

    Implement L3. State Management/3. Managing State

--- React-Fundamentals-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
+++ React-Fundamentals-Exercises/L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,22 @@
 import corgi from "./corgi.jpg";
+import React, { useState } from 'react';
 import "./App.css";
 
 const App = () => {
+  const [likeCount, setLikeCount] = useState(0);
+  const corgiClickCallback = () => {
+    setLikeCount(likeCount+1);
+  }
+  const resetLikeCount = () => {
+    setLikeCount(0);
+  }
   return (
     <div className="container">
       <h2>Like this photo!</h2>
-      <img src={corgi} alt="Corgi"></img>
+      <img src={corgi} alt="Corgi" onClick={(event) => corgiClickCallback()}></img>
       <p>Amount of likes: </p>
+      <p>{likeCount}</p>
+      <button onClick={(event) => resetLikeCount()}>Reset Like Count</button>
     </div>
   );
 };
