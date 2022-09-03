796c78a54316200ad4a0ff142268f85775b40fbc
Nick Anthony <nickanthony45@gmail.com>
2022-09-03 11:41:43 -0400

    L3. State Management/6. All Together: Add show/hide games played

--- React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/User.js
+++ React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/User.js
@@ -1,17 +1,19 @@
 import PropTypes from 'prop-types';
 
-const User = ({ user }) => {
+const User = ({ user, showGamesPlayed }) => {
+  const gamesPlayed = showGamesPlayed ? user.games_played : 0;
   return (
         <li key={user.username} className="user">
           <p>{user.first_name} {user.last_name}</p>
           <p>{user.username}</p>
-          <p>{user.username} played {user.games_played} games.</p>
+          <p>{user.username} played {gamesPlayed} games.</p>
         </li>
   );
 }
 
 User.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
+  showGamesPlayed: PropTypes.func.isRequired,
 };
 
 export default User;
--- React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/UserList.js
+++ React-Fundamentals-Exercises/L3. State Management/6. All Together/Starter/src/UserList.js
@@ -1,14 +1,33 @@
 import PropTypes from "prop-types";
 import User from "./User.js";
+import { useState } from "react";
 
 const UserList = ({ users }) => {
+  const [showGamesPlayed, setShowGamesPlayed] = useState(true);
+
+  const handleShowGamesPlayChange = () => {
+    setShowGamesPlayed(!showGamesPlayed);
+  }
+
+  const buttonValue = showGamesPlayed ? "Hide games played" : "Show games played";
+
   return (
-    <ul className="user-list">
-      {users.map((user) => (
-        <User key={user.username} user={user} />
-      ))
-      }
-    </ul>
+    <div>
+      <button
+        className="show-hide-games-played-button"
+        onClick={handleShowGamesPlayChange}>
+          {buttonValue}
+      </button>
+      <ul className="user-list">
+        {users.map((user) => (
+          <User
+            key={user.username}
+            user={user}
+            showGamesPlayed={showGamesPlayed}/>
+        ))
+        }
+      </ul>
+    </div>
   );
 }
 
