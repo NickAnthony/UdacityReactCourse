2da5dfc494d27e7514e14de67fc750cab498477f
Nick Anthony <nickanthony45@gmail.com>
2022-09-05 07:23:04 -0400

    Implement clear timer in L4. Hooks/3. Side Effect Cleanup
    This works by cancelling the timer async call when the
    component is unmounted.  Thus the callback never executes
    after unmounting.

--- React-Fundamentals-Exercises/L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
+++ React-Fundamentals-Exercises/L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
@@ -4,7 +4,15 @@ function ChildComponent() {
   const [color, setColor] = useState("red");
 
   useEffect(() => {
-    setTimeout(() => setColor("green"), 3000);
+    // greenTimer is the timer ID.  We save it so we can also clean it up
+    // when the component is unmounted.
+    const greenTimer = setTimeout(() => setColor("green"), 3000);
+
+    // Return a clean up function that cancels the timer in the case the
+    // component in unmounted.
+    return function cleanup() {
+      clearTimeout(greenTimer);
+    };
   });
 
   return <p style={{ color }}>{color}</p>;
